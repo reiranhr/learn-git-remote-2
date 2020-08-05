746a47c4a4fa1f15d851e96db690e665528bded4
Alisha Yumna Bakri <alishayumnab@gmail.com>
2020-08-05 09:38:08 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1> Hello world </h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias hic placeat obcaecati aspernatur animi distinctio incidunt eius. Consequatur iste facilis omnis harum accusantium maiores porro et nesciunt rem! Maxime doloremque placeat fuga ullam architecto reprehenderit temporibus blanditiis quos nam corporis, dicta reiciendis ipsam sequi, iste saepe ducimus porro excepturi itaque! </p>
 </body>
 </html>
