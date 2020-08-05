17f98a4db63a41e831addf4a1d8cf6a7f3afac1d
Alisha Yumna Bakri <alishayumnab@gmail.com>
2020-08-05 09:43:29 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1> Hello world </h1>
     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias hic placeat obcaecati aspernatur animi distinctio incidunt eius. Consequatur iste facilis omnis harum accusantium maiores porro et nesciunt rem! Maxime doloremque placeat fuga ullam architecto reprehenderit temporibus blanditiis quos nam corporis, dicta reiciendis ipsam sequi, iste saepe ducimus porro excepturi itaque! </p>
+    <ul>
+        <li> List 1 </li>
+        <li> List 2 </li>
+    </ul>
 </body>
 </html>
